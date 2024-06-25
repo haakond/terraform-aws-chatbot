a8bd5199e06a694dc3b625b31520f8431f01c2e6
Håkon Eriksen Drange <hakon.drange@soprasteria.com>
2024-06-25 13:25:54 +0200

    Update examples.

--- examples/main.tf
+++ examples/main.tf
@@ -1,4 +1,13 @@
-module "aws_chatbot" {
-  # Check https://github.com/haakond/terraform-aws-chatbot/commits/main/ for the latest hash and update accordingly.
-  source = "git::https://github.com/haakond/terraform-aws-chatbot.git?ref=250fa4335eaff852ddc7d9fc40b7e62aea8ab1e6"
+# Find relevant commit hash from https://github.com/haakond/terraform-aws-chatbot/commits/main/
+module "aws_chatbot_slack" {
+  source = "git::https://github.com/haakond/terraform-aws-chatbot.git?ref=f769d98ff5495819290bbd82dd53f40d711e4cfb"
+  providers = {
+    aws           = aws
+    aws.us-east-1 = aws.us-east-1
+    awscc         = awscc
+  }
+  slack_channel_configuration_name = "slack-hed-aws-monitoring"
+  slack_channel_id                 = "AABBCC001122DD88"
+  slack_workspace_id               = "ABCD1234EFGH5678"
+  logging_level                    = "INFO"
 }
