cdff5b6bf40053b7632e6bdf765bdf0b87bdf925
Håkon Eriksen Drange <hakon.drange@soprasteria.com>
2024-06-25 13:29:01 +0200

    Update examples - hash.

--- examples/main.tf
+++ examples/main.tf
@@ -1,6 +1,6 @@
 # Find relevant commit hash from https://github.com/haakond/terraform-aws-chatbot/commits/main/
 module "aws_chatbot_slack" {
-  source = "git::https://github.com/haakond/terraform-aws-chatbot.git?ref=f769d98ff5495819290bbd82dd53f40d711e4cfb"
+  source = "git::https://github.com/haakond/terraform-aws-chatbot.git?ref=a8bd5199e06a694dc3b625b31520f8431f01c2e6"
   providers = {
     aws           = aws
     aws.us-east-1 = aws.us-east-1
